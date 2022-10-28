b7a2ae93dfa79eeafe77ebceec2ab1a34c141c98
Tom Lin <tom91136@gmail.com>
2022-10-28 01:38:14 +0100

    Add API generation 2

--- action_generate_api.sh
+++ action_generate_api.sh
@@ -7,8 +7,9 @@ OUT=$2
 builds="builds-$VARIANT.json"
 readarray -t keys < <(jq -r 'keys | .[]' "$builds")
 mkdir -p "$OUT/$VARIANT"
+echo "Writing ${#keys[@]} entries to $OUT/$VARIANT"
 for key in "${keys[@]}"; do
-  jq -c ".\"$key\"" "$builds" >"$OUT/$VARIANT/$key.json" &
+  jq -c ".\"$key\"" "$builds" >"$OUT/$VARIANT/$key.json" && echo "Wrote ${key}.json" &
 done
 wait
 echo "Done"
