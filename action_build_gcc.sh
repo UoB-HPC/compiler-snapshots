6e607be15c4b7cf07556bdb3a53cefadc59c1e76
Tom Lin <tom91136@gmail.com>
2025-08-20 04:37:21 +0100

    Fix bad asan in gcc<=9

--- action_build_gcc.sh
+++ action_build_gcc.sh
@@ -25,6 +25,10 @@ for build in "${builds_array[@]}"; do
 
   hash=$(jq -r ".\"$build\" | .hash" "/host/builds.json")
 
+  # Commits before d5ca27efb4b69f8fdf38240ad62cc1af30a30f77 requires an old glibc for asan to work
+  # See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=113181
+  SAN_FIX=d5ca27efb4b69f8fdf38240ad62cc1af30a30f77
+
   echo "Build   : $build"
   echo "Commit  : $hash"
 
@@ -34,11 +38,30 @@ for build in "${builds_array[@]}"; do
     --prune \
     --progress \
     --no-recurse-submodules \
-    --depth=1 \
-    origin "$hash"
+    --filter=blob:none \
+    origin "$hash" "$SAN_FIX"
 
-  git reset
-  git checkout FETCH_HEAD
+  git checkout -q "$hash"
+
+  has_san_fix=0
+  if git merge-base --is-ancestor "$SAN_FIX" "$hash"; then
+    has_san_fix=0
+  else
+    rc=$?
+    if [ "$rc" -eq 1 ]; then
+      has_san_fix=1
+    else
+      echo "Unable to determine ancestry (git merge-base failed with code $rc); assuming fix present."
+      has_san_fix=0
+    fi
+  fi
+
+  if [ "$has_san_fix" -ne 0 ]; then
+    extra="--disable-libsanitizer"
+  else
+    echo "Commit does not require disabling support, continuing..."
+    extra=""
+  fi
 
   echo "Source cloned, starting build step..."
 
@@ -58,8 +81,14 @@ for build in "${builds_array[@]}"; do
     (
       cd build
       ../configure \
-      CFLAGS=-Wno-error=incompatible-pointer-types \
-      --prefix="/opt/$build" --enable-languages=c,c++,fortran --disable-bootstrap --disable-multilib --disable-libvtv --without-isl
+        CFLAGS=-Wno-error=incompatible-pointer-types \
+        --prefix="/opt/$build" \
+        --enable-languages=c,c++,fortran \
+        --disable-bootstrap \
+        --disable-multilib \
+        --disable-libvtv \
+        --without-isl \
+        $extra
     )
     time make -C build -j "$(nproc)"
     time make -C build -j "$(nproc)" install DESTDIR="$dest_dir"
